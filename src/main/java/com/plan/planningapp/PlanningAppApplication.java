293c62fca5dc5958c5ba2302b08b284838b1732b
cyanplatypus <julia.a.andr@gmail.com>
2018-03-11 21:39:16 +0300

    test

---
+++ src/main/java/com/plan/planningapp/PlanningAppApplication.java
@@ -0,0 +1,12 @@
+package com.plan.planningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningAppApplication.class, args);
+	}
+}
---
+++ src/main/java/com/plan/planningapp/controller/PlanController.java
@@ -0,0 +1,41 @@
+package com.plan.planningapp.controller;
+
+import classes.UserDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping(path = "/plan")
+public class PlanController {
+
+    @CrossOrigin
+    @GetMapping(path = "hello")
+    public @ResponseBody String SayHello(){
+        return "Oh hi Mark";
+    }
+
+    @CrossOrigin
+    @GetMapping(path = "hello")
+    public @ResponseBody List<UserDto> GetUsers(){
+        List<UserDto> list = new ArrayList<>();
+
+        UserDto bob = new UserDto();
+        bob.setFirstName("Bob");
+        bob.setEmailAcc("bluebird@bob.com");
+
+        UserDto alice = new UserDto();
+        alice.setFirstName("Alice");
+        alice.setEmailAcc("bluebird_2@bob.com");
+
+        list.add(bob);
+        list.add(alice);
+
+        return list;
+    }
+}
